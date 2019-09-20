2f7919629115f423097aa6766b5943152bdb8756
codbear <douglas.meunier@codbear.com>
2019-09-20 14:14:10 +0200

    core(router): create a route for login page

--- public/index.php
+++ public/index.php
@@ -4,7 +4,19 @@ require_once('Autoloader.php');
 Autoloader::registerControllers();
 
 try {
-	$controller = new HomeController();
+	if (isset($_GET['view'])) {
+		switch ($_GET['view']) {
+			case 'login':
+				$controller = new LoginController();
+				break;
+
+			default:
+				// code...
+				break;
+		}
+	} else {
+		$controller = new HomeController();
+	}
 	$controller->execute($_GET);
 } catch (\Exception $e) {
 	$errorMessage = $e->getMessage();
