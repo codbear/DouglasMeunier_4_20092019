90bcb7a4f44d1d6c2a092a91a4a8f76efbf0e48a
codbear <douglas.meunier@codbear.com>
2019-09-20 15:17:19 +0200

    core(controller): add posts datas

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,7 +4,7 @@ require_once('../app/interfaces/ControllerInterface.php');
 
 class HomeController implements ControllerInterface {
 
-	public function execute($params) {
+	public function execute($params, $datas) {
 		$title = 'Un billet pour l\'Alaska';
 		require_once('../app/views/home.php');
 	}
