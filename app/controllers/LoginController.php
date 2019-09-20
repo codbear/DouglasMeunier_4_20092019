3c1818a4612f64ccc81bd20b762ea44c8349f767
codbear <douglas.meunier@codbear.com>
2019-09-20 15:15:08 +0200

    core(controller): create a function to register user

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,10 +1,33 @@
 <?php
 
 require_once('../app/interfaces/ControllerInterface.php');
+require_once('../app/models/LoginModel.php');
 
 class LoginController implements ControllerInterface {
 
-	public function execute($params) {
+	private function registerUser($datas) {
+		$username = $datas['username'];
+		$email = $datas['email'];
+		$password = password_hash($datas['password'], PASSWORD_DEFAULT);
+		$role = 2;
+		$login = new LoginModel();
+		$register = $login->registerUser($username, $email, $password, $role);
+	}
+
+	public function execute($params, $datas) {
+		$title = 'Un billet pour l\'Alaska - Connexion / Inscription';
+
+		if (isset($params['action'])) {
+			switch ($params['action']) {
+				case 'register':
+					$this->registerUser($datas);
+					break;
+
+				default:
+					// code...
+					break;
+			}
+		}
 		require_once('../app/views/login.php');
 	}
 }
