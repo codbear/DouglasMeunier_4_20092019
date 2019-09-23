22e9fe197e3eb4a1669ea04e0cf022a10b29085f
codbear <douglas.meunier@codbear.com>
2019-09-23 22:39:03 +0200

    refactor(template-view): remove unnecessary div

--- app/views/template.php
+++ app/views/template.php
@@ -10,9 +10,7 @@
 
     <body>
 		<?php include('menu.php');?>
-        <div class="row">
-            <?php echo $content; ?>
-        </div>
+		<?php echo $content; ?>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </body>
 </html>
