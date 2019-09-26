f3a6fc482d814ebefbf771d2f1297e08671276b7
codbear <douglas.meunier@codbear.com>
2019-09-26 17:49:15 +0200

    core(error-404): set headers to 404 not found for page 404

--- app/controllers/ErrorsController.php
+++ app/controllers/ErrorsController.php
@@ -6,6 +6,7 @@ class ErrorsController implements ControllerInterface {
 
 	public function execute($params, $datas) {
 		$title = 'Erreur 404';
+        header("HTTP/1.0 404 Not Found");
 		require_once('../app/views/404.php');
 	}
 }
