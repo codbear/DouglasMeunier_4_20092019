eb4fac4a1bfd86ad62f0becb385bc1997b3c8485
codbear <douglas.meunier@codbear.com>
2019-09-26 16:03:52 +0200

    core(router): create Router class

---
+++ public/Router.php
@@ -0,0 +1,26 @@
+<?php
+
+class Router {
+
+    public static function init() {
+        try {
+        	if (isset($_GET['view'])) {
+        		switch ($_GET['view']) {
+        			case 'login':
+        				$controller = new LoginController();
+        				break;
+
+        			default:
+        				$controller = new ErrorsController();
+        				break;
+        		}
+        	} else {
+        		$controller = new HomeController();
+        	}
+        	$controller->execute($_GET, $_POST);
+        } catch (\Exception $e) {
+        	$errorMessage = $e->getMessage();
+        	echo $errorMessage;
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -3,24 +3,6 @@
 session_start();
 
 require_once('Autoloader.php');
+require_once('Router.php');
 Autoloader::registerControllers();
-
-try {
-	if (isset($_GET['view'])) {
-		switch ($_GET['view']) {
-			case 'login':
-				$controller = new LoginController();
-				break;
-
-			default:
-				$controller = new ErrorsController();
-				break;
-		}
-	} else {
-		$controller = new HomeController();
-	}
-	$controller->execute($_GET, $_POST);
-} catch (\Exception $e) {
-	$errorMessage = $e->getMessage();
-	echo $errorMessage;
-}
+Router::init();
