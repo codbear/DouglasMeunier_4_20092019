76fe95a44505f46f61317f7d0068031b83606941
codbear <douglas.meunier@codbear.com>
2019-10-22 01:00:13 +0200

    core(controllers): create controller to load admin dashboard

---
+++ App/Controllers/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Codbear\Alaska\Controllers;
+
+use Codbear\Alaska\Interfaces\ControllerInterface;
+
+class DashboardController implements ControllerInterface {
+
+    public function execute($params, $datas) {
+		$title = 'Dashboard';
+		require_once('../App/Views/dashboard.php');
+	}
+
+}
