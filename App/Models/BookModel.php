515f591967cf1c9cd0084a5e97b4f23c7b1071b4
codbear <douglas.meunier@codbear.com>
2019-10-22 08:05:47 +0200

    core(BookModel): add a function to get list of all chapters

--- App/Models/BookModel.php
+++ App/Models/BookModel.php
@@ -6,6 +6,12 @@ use Codbear\Alaska\Models\DatabaseModel;
 
 class BooksModel extends DatabaseModel {
 
-
+    public function getTableOfContent() {
+        $db = $this->dbConnect();
+        $req = $db->query('SELECT id, title, DATE_FORMAT(creation_date, \'%d/%m/%Y\' AS creation_date_fr
+                            FROM posts
+                            ORDER BY creation_date');
+        return $req;
+    }
     
 }
