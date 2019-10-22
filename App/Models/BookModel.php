5f23f843d7ad19e467e1b398657ab0c3fea3d241
codbear <douglas.meunier@codbear.com>
2019-10-22 08:21:19 +0200

    fix(BookModel): rename class properly and close bracket in db query

--- App/Models/BookModel.php
+++ App/Models/BookModel.php
@@ -4,11 +4,11 @@ namespace Codbear\Alaska\Models;
 
 use Codbear\Alaska\Models\DatabaseModel;
 
-class BooksModel extends DatabaseModel {
+class BookModel extends DatabaseModel {
 
     public function getTableOfContent() {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT id, title, DATE_FORMAT(creation_date, \'%d/%m/%Y\' AS creation_date_fr
+        $req = $db->query('SELECT id, title, DATE_FORMAT(creation_date, \'%d/%m/%Y\') AS creation_date_fr
                             FROM posts
                             ORDER BY creation_date');
         return $req;
