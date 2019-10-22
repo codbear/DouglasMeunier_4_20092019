afa9e6089bc547a63269de192d480dc7146395ba
codbear <douglas.meunier@codbear.com>
2019-10-22 08:15:44 +0200

    refactor(DashboardController): create a subrouter and add a function to load chapters admin panel

--- App/Controllers/DashboardController.php
+++ App/Controllers/DashboardController.php
@@ -2,13 +2,32 @@
 
 namespace Codbear\Alaska\Controllers;
 
+use Codbear\Alaska\Models\BookModel;
 use Codbear\Alaska\Interfaces\ControllerInterface;
 
 class DashboardController implements ControllerInterface {
 
+    public function loadChaptersAdminPanel() {
+        $book = new BookModel();
+        $title = 'Dashboard - Chapitres';
+        $chaptersList = $book->getTableOfContent();
+        require_once('../App/Views/chapters.php');
+    }
+
     public function execute($params, $datas) {
-		$title = 'Dashboard';
-		require_once('../App/Views/chapters.php');
+        if (isset($params['section'])) {
+			switch ($params['section']) {
+                case 'chapters':
+					$this->loadChaptersAdminPanel();
+					break;
+
+				default:
+                    require_once('../App/Views/404.php');
+					break;
+			}
+		} else {
+			require_once('../App/Views/404.php');
+		}
 	}
 
 }
