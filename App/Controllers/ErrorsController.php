71186671413683d8039efd9580c1f2aa4e9a3832
codbear <douglas.meunier@codbear.com>
2019-10-28 22:50:56 +0100

    refactor(ErrorControler): create new functions for errors 403 and 404

--- App/Controllers/ErrorsController.php
+++ App/Controllers/ErrorsController.php
@@ -6,9 +6,19 @@ use Codbear\Alaska\Interfaces\ControllerInterface;
 
 class ErrorsController implements ControllerInterface {
 
-	public function execute($params, $datas) {
+	public static function error403() {
+		$title = 'Erreur 403';
+        header("HTTP/1.0 403 Forbidden");
+		require_once('../App/Views/Errors/error403.php');
+	}
+
+	public static function error404() {
 		$title = 'Erreur 404';
         header("HTTP/1.0 404 Not Found");
-		require_once('../App/Views/404.php');
+		require_once('../App/Views/Errors/error404.php');
+	}
+
+	public function execute($params, $datas) {
+		$this->error404();
 	}
 }
